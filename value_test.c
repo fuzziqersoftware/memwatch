299a8d2a48bac4c49a986fba3d5323ca7b46d4e8
Fuzziqer Software <fuzziqersoftware@pointy.local>
2012-03-25 21:46:56 -0700

    change value_test a bit

--- value_test.c
+++ value_test.c
@@ -4,20 +4,17 @@
 
 int main(int argc, char* argv[]) {
 
-  //int8_t s8 = 0;
-  //int16_t s16 = 0;
+  int8_t s8 = 0;
+  int16_t s16 = 0;
   int32_t s32 = 0;
-  //int64_t s64 = 0;
+  int64_t s64 = 0;
 
   for (;;) {
     printf("enter value: ");
-    scanf("%d", &s32);
-    //s8 = s16 = s32 = s64;
+    scanf("%lld", &s64);
+    s8 = s16 = s32 = s64;
 
-    //printf("s8  = %hhd\n", s8);
-    //printf("s16 = %hd\n", s16);
-    printf("s32 = %d\n", s32);
-    //printf("s64 = %lld\n", s64);
+    printf("8/16/32/64: %hhd / %hd / %d / %lld\n", s8, s16, s32, s64);
   }
 
   return 0;
