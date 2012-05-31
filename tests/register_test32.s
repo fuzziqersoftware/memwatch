f259c55ed2077b7c01077ab18c4799c017b1292d
Martin Michelsen <mjem@wildblue.net>
2012-05-30 17:51:28 -0700

    make register_test* do PT_DENY_ATTACH and delete pt_deny_test

--- tests/register_test32.s
+++ tests/register_test32.s
@@ -1,12 +1,24 @@
 // value_test_float.c, by Martin Michelsen, 2012
 // simple app that sets its regs to a certain value, then sleeps
 
-.intel_syntax
+.intel_syntax noprefix
 
 .globl main
 .globl _main
 main:
 _main:
+
+  push 0
+  push 0
+  mov eax, 20
+  int 0x80 // getpid()
+  push eax
+  push 31 // PT_DENY_ATTACH
+  mov eax, 26
+  push eax
+  int 0x80 // ptrace()
+  add esp, 20
+
   mov eax, 0x54389843
   //// eax = 0x0000000054389843
 
--- tests/register_test64.s
+++ tests/register_test64.s
@@ -1,12 +1,24 @@
 // value_test_float.c, by Martin Michelsen, 2012
 // simple app that sets its regs to a certain value, then sleeps
 
-.intel_syntax
+.intel_syntax noprefix
 
 .globl main
 .globl _main
 main:
 _main:
+
+  mov rax, 0x02000014 // getpid
+  syscall
+
+  mov rdi, 31 // PT_DENY_ATTACH
+  mov rsi, rax // pid
+  xor rdx, rdx // NULL
+  xor rcx, rcx // 0
+  mov rax, 0x0200001A // ptrace
+  syscall
+  //// ptrace(PT_DENY_ATTACH, getpid(), NULL, 0)
+
   xor rax, rax
   mov eax, 0x54389843
   //// rax = 0x0000000054389843
